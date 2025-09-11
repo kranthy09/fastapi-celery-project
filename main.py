e06bc494d9ef77dc4c9698e21ff3888e4c0ba6be
kranthu <g.kranthi2507@gmail.com>
2025-09-11 12:41:44 +0530

    user table

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-from celery import Celery
 from project import create_app
 
 app = create_app()
-
-
-celery = Celery(
-    __name__,
-    broker="redis://127.0.0.1:6379/0",
-    backend="redis://127.0.0.1:6379/0",
-)
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@celery.task
-def divide(x, y):
-    import time
-
-    time.sleep(5)
-    return x / y
+celery = app.celery_app
