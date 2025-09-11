effff69ed133e0218d827897220354ac489f935a
kranthu <g.kranthi2507@gmail.com>
2025-09-12 05:23:26 +0530

    webscokets and celery

--- project/config.py
+++ project/config.py
@@ -17,6 +17,9 @@ class BaseConfig:
         "CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/0"
     )  # NEW
     CELERY_TASK_ALWAYS_EAGER: bool = False
+    WS_MESSAGE_QUEUE: str = os.environ.get(
+        "WS_MESSAGE_QUEUE", "redis://127.0.0.1:6379/0"
+    )
 
 
 class DevelopmentConfig(BaseConfig):
