e24a766dcea4584ab10315f7ea716ab407aefa14
kranthu <g.kranthi2507@gmail.com>
2025-09-12 01:31:58 +0530

    refactor

--- project/users/tasks.py
+++ project/users/tasks.py
@@ -1,9 +1,10 @@
 from celery import shared_task
-from celery.contrib import rdb
 
 
 @shared_task
 def divide(x, y):
+
+    # from celery.contrib import rdb
     # rdb.set_trace()
 
     import time
